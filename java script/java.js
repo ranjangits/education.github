0a5c17cd7685137e580db15485ebb2a4d19706ad
ranjangits <rk9122439340@gmail.com>
2022-03-06 21:11:03 +0530

    initial commit

---
+++ java script/java.js
@@ -0,0 +1,29 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = ` 
+    
+        
+ 
+            <a href='education.html' class="bas">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+        
+            <a href='java.html' class="bas">JAVA</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+            <a href='html.html' class="bas">HTML</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+            <a href='css.html' class="bas">CSS</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+     
+        `
+
+    }
+}
+customElements.define('my-header', MyHeader)
+
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        Copyright (c) 2022 Ranjan kumar 
+        
+        `
+
+    }
+}
+customElements.define('my-footer', MyFooter)
